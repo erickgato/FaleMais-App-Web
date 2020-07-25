da46ee56d44933149fb7d7764c99f4dbcece9cf6
erickgato <itsbk0703@gmail.com>
2020-07-25 16:51:37 -0300

    Main containers

---
+++ app/src/components/main/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../stylizations/about.scss'
+export default class About extends React.Component {
+    render() {
+        return (
+           <div className="Content" >
+               <div className="fleft">
+                    Com os planos fale mais você pode ligar a vontade!!
+               </div>
+               <div className="fright">
+                    Telzir lanca um novo produto para voce 
+                    exemplo exemplo 
+               </div>
+           </div>
+        )
+    }
+}
