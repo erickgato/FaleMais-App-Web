64f088f51a5552aa2b1b25524f6362c10187d65d
erickgato <itsbk0703@gmail.com>
2020-07-26 15:34:52 -0300

    Added 'Preços' title

--- app/src/components/menu.jsx
+++ app/src/components/menu.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom'
 
 export default class Menu extends Component { 
     render() {
-        return (
-            <div className="menu">
+        const classname = `menu ${this.props.transparent === true ? 'transparent' : '' }`;
+        return (      
+            <div className={classname}>
                 <div className="title">
                     <span><Link to="/"> Telzir </Link></span>
                 </div>
                 <nav>
                     <ul>
-                        <li >  <Link to="/">  PLANS </Link> </li>
-                        <li > <Link to="/about"> ABOUT </Link>  </li>
+                        <li >  <Link to="/prices">  PREÇOS </Link> </li>
+                        <li > <Link to="/about"> SOBRE </Link>  </li>
                     </ul>
                 </nav>
             </div>
