a359da56a6feee71a5011cd2c2b67901e3dc10ee
erickgato <itsbk0703@gmail.com>
2020-07-26 15:33:30 -0300

    Route prices

--- app/src/Routes/index.jsx
+++ app/src/Routes/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router'
 import Home from '../components/mainPage'
 import About from '../components/main/about'
+import Prices from '../components/main/prices'
 export default props => 
                         <Switch>
                             <Route exact path="/" component={Home}/>
                             <Route path="/about" component={About}/>
+                            <Route path="/prices" component={Prices}/>
                             <Redirect from="*" to="/" />
                         </Switch>
 
